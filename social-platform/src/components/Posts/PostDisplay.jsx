d26172ba8b19bd99a5a4860ca8e583bb47923bac
ShiraPliskin <shirap803@gmail.com>
2024-01-21 02:41:45 +0200

    style....

--- social-platform/src/components/Posts/PostDisplay.jsx
+++ social-platform/src/components/Posts/PostDisplay.jsx
@@ -20,32 +20,24 @@ const PostDisplay = ({ postToDisplay, setPosts, posts, setCommentArea, personalO
         <div>
             <ul className='list'>
                 {personalOrOtherPosts === "other" &&
-                    <li>
-                        <strong>User Id:</strong> {postToDisplay.userId}
-                    </li>
+                    <li><strong>User Id:</strong> {postToDisplay.userId} </li>
                 }
-                <li>
-                    <strong>id:</strong> {postToDisplay.id}
-                </li>
-                <li>
-                    <strong>title:</strong> {inUpdate ?
+                <li><strong>id:</strong> {postToDisplay.id}</li>
+                <li><strong>title:</strong> {inUpdate ?
                         <textarea type="text" value={updatedTitle} onChange={(event) => { setUpdatedTitle(event.target.value) }} required style={{ width: "350px" }} /> :
-                        postToDisplay.title}
-                </li>
-                {showMore && (
-                    <li>
-                        <strong>body:</strong> {inUpdate ?
+                        postToDisplay.title}</li>
+                {showMore && 
+                    <li><strong>body:</strong> {inUpdate ?
                             <textarea type="text" value={updatedBody} onChange={(event) => { setUpdatedBody(event.target.value) }} required style={{ width: "350px", height: "70px" }} /> :
-                            postToDisplay.body}
-                    </li>
-                )}
+                            postToDisplay.body}</li>
+                }
             </ul>
             <strong><button className="actionButton" onClick={() => setShowMore(!showMore)}>{showMore ? '-' : '+'}</button></strong>
             {personalOrOtherPosts === "personal" &&
                 <><PostUpdate inUpdate={inUpdate} setInUpdate={setInUpdate} postToUpdate={postToDisplay} setCommentArea={setCommentArea}
                     setPosts={setPosts} posts={posts} updatedTitle={updatedTitle} updatedBody={updatedBody} />
                     <PostRemove postToRemove={postToDisplay} setCommentArea={setCommentArea} setPosts={setPosts} posts={posts} /><br /><br /></>}
-            {showMore && <button className="actionButton" onClick={() => setShowComments((prev) => !prev)}>{showComments ? 'hide comments' : 'Show Comments'}</button>}
+            {showMore && <button className="actionButton" onClick={() => setShowComments((prev) => !prev)}>{showComments ? 'Hide comments' : 'Show Comments'}</button>}
             {showComments && showMore && <Comments postId={postToDisplay.id} />}
         </div>
     </>
